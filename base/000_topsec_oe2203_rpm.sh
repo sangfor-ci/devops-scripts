78ebb35f5456bf734af957f3c1629f0b4a9cc6b1
xx-zhang <acatanble@aliyun.com>
2023-01-08 20:28:15 +0800

    -fix

--- base/000_topsec_oe2203_rpm.sh
+++
@@ -1,9 +0,0 @@
-#!//bin/bash
-
-BCI_HOST='10.7.202.199'
-MIRROR_HOST='10.2.136.110'
-
-sed -i 's/repo.openeuler.org/10.2.136.110\/openeuler/g' /etc/yum.repos.d/openEuler.repo
-cat /etc/yum.repos.d/openEuler.repo
-
-yum makecache 
--- base/001_disable_selinux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Disable SELinux and firewalld
-echo ">>> Disable SELinux and firewalld"
-setenforce 0
-sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
-systemctl disable --now firewalld >/dev/null 2>&1
